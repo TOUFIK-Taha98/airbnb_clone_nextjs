74c621fec22447a3e0aae861057a5e7538119334
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2024-11-12 11:53:42 +0100

    Fetch Explore data in server side

--- app/components/Explore/ExploreNearby.tsx
+++ app/components/Explore/ExploreNearby.tsx
@@ -1,10 +1,28 @@
 import ExploreCard from "@/app/components/Cards/ExploreCard";
+import { ExploreData } from "@/app/types/app";
+import { getExplore } from "@/app/utils/api";
+import MainHeading from "@/app/components/MainHeading";
 
-const ExploreNearby = () => {
+const ExploreNearby = async () => {
+  console.log("Entering ExploreNearby");
+
+  const exploreData: ExploreData = await getExplore();
+
+  console.log(exploreData);
+
   return (
-    <section className="pt-6">
-      <h2 className="text-4xl font-semibold mb-5">I am a explore</h2>
-      <ExploreCard />
+    <section className="ml-6 pt-6">
+      <MainHeading title="Explore Nearby" />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {exploreData.map((item) => (
+          <ExploreCard
+            key={item.img}
+            img={item.img}
+            distance={item.distance}
+            location={item.location}
+          />
+        ))}
+      </div>
     </section>
   );
 };
